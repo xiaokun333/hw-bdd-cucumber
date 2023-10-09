ef0c4eb3c9b1da7b05fccc09547ab3c5436092ee
Xiaokun Yu <xy2550@columbia.edu>
2023-10-09 17:54:55 +0000

    writing part 1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,9 +1,11 @@
 # Add a declarative step here for populating the DB with movies.
 
+
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
   pending "Fill in this step in movie_steps.rb"
 end
